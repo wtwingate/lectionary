6ff1c47ebed51c71e0b5ab23d58de13906e750d6
William Wingate <wtwingate@gmail.com>
2024-08-22 11:53:15 -0400

    Set up views and routes for Days

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,7 +1,35 @@
 class DaysController < ApplicationController
   def index
+    start_date = Date.today()
+    unless params[:start_date].nil? || params[:start_date].empty?
+      start_date = Date.parse(params[:start_date])
+    end
+
+    end_date = start_date + 4.weeks
+    unless params[:end_date].nil? || params[:end_date].empty?
+      end_date = Date.parse(params[:end_date])
+    end
+
+    dates = (start_date..end_date).to_a
+    calendars = dates.map { |date| Calendar.new(date) }
+
+    days = []
+    calendars.each do |calendar|
+      next if calendar.days.empty?
+
+      calendar.days.each do |name|
+        matches = Day.where(name: name, year: calendar.year)
+        matches.each do |match|
+          match[:season] = calendar.season if match[:season].nil?
+        end
+        days += matches
+      end
+    end
+
+    @days = days
   end
 
   def show
+    @day = Day.find(params[:id])
   end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,8 @@ class Lesson < ApplicationRecord
   belongs_to :day
 
   validates :references, presence: true
+
+  def print_references
+    self.references.join(" or ")
+  end
 end
