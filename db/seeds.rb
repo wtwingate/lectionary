a00c14b3270d7639123d088fe08f43f6f03f2cb5
William Wingate <wtwingate@gmail.com>
2024-08-22 11:24:49 -0400

    Seed database with lectionary data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "json"
+
+lectionary_file = File.open("db/lectionary.json").read
+lectionary_data = JSON.parse(lectionary_file)
+
+lectionary_data.each do |data|
+  day = Day.find_or_create_by!(
+    name: data["name"],
+    service: data["service"],
+    year: data["year"],
+    season: data["seasons"],
+    color: data["color"],
+    rank: data["rank"]
+  )
+
+  data["collects"].each do |collect|
+    day.collects.find_or_create_by!(
+      text: collect
+    )
+  end
+
+  data["lessons"].each do |lesson|
+    day.lessons.find_or_create_by!(
+      references: lesson
+    )
+  end
+end
