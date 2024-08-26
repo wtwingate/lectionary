8d7ba6e2c267609bf464cf3575be39ba10694652
William Wingate <wtwingate@gmail.com>
2024-08-26 12:31:42 -0400

    Add FontAwesome icons with importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,8 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
+# pin "@fortawesome/fontawesome-free", to: "@fortawesome--fontawesome-free.js" # @6.6.0
+pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.6.0/js/all.js"
+
 pin_all_from "app/javascript/controllers", under: "controllers"
